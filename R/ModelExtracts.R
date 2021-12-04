b8cb8dd0d63cdf9c9273f0cb9a907e5e89f3288f
James Turner <52140798+Turner-JA@users.noreply.github.com>
2021-12-04 01:42:49 +0000

    Add files via upload

---
+++ R/ModelExtracts.R
@@ -0,0 +1,44 @@
+
+Extract_LMER<-function(Mod){
+  datOut = as.data.frame(summary(Mod)$coef)
+  datOut[,1:4]=round(datOut[,1:4], digits=2)
+  names(datOut)[names(datOut) == "Pr(>|t|)"] <- "p_value"
+  datOut$p_value=as.numeric(as.character(datOut$p_value))
+  names(datOut)[names(datOut) == "t value"] <- "t_value"
+  datOut<-tibble::rownames_to_column(datOut, "Fixed_Factors")
+  datOut$Significance[is.na(datOut$p_value)==F]="n.s."
+  datOut$Significance[datOut$p_value<.1]=" ."
+  datOut$Significance[datOut$p_value<.05]=" *"
+  datOut$Significance[datOut$p_value<.01]=" **"
+  datOut$Significance[datOut$p_value<.001]=" ***"
+  names(datOut)[names(datOut) == "t_value2"] <- "t value"
+  names(datOut)[names(datOut) == "Fixed_Factors"] <- "Fixed_Effects"
+  names(datOut)[names(datOut) == "Std. Error"] <- "SE"
+  datOut$p_value=format(round(datOut$p_value, digits=3), nsmall=3)
+  colnames(datOut)[colnames(datOut)=="p_value"]="p value"
+  colnames(datOut)[colnames(datOut)=="t_value"]="t value"
+  print(datOut)
+  write.csv(datOut, file = "LMERModelOutput.csv")
+}
+
+Extract_GLMER<-function(Mod){
+  datOut = as.data.frame(summary(Mod)$coef)
+  datOut[,1:3]=round(datOut[,1:3], digits=2)
+  names(datOut)[names(datOut) == "Pr(>|z|)"] <- "p_value"
+  datOut$p_value=as.numeric(as.character(datOut$p_value))
+  names(datOut)[names(datOut) == "z value"] <- "z_value"
+  datOut<-tibble::rownames_to_column(datOut, "Fixed_Factors")
+  datOut$Significance[is.na(datOut$p_value)==F]="n.s."
+  datOut$Significance[datOut$p_value<.1]=" ."
+  datOut$Significance[datOut$p_value<.05]=" *"
+  datOut$Significance[datOut$p_value<.01]=" **"
+  datOut$Significance[datOut$p_value<.001]=" ***"
+  names(datOut)[names(datOut) == "t_value2"] <- "t value"
+  names(datOut)[names(datOut) == "Fixed_Factors"] <- "Fixed_Effects"
+  names(datOut)[names(datOut) == "Std. Error"] <- "SE"
+  datOut$p_value=format(round(datOut$p_value, digits=3), nsmall=3)
+  colnames(datOut)[colnames(datOut)=="p_value"]="p value"
+  colnames(datOut)[colnames(datOut)=="t_value"]="t value"
+  print(datOut)
+  write.csv(datOut, file = "GLMERModelOutput.csv")
+}
